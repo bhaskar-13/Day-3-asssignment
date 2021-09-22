7f7671edd969b106f390e5ec757eca4f02bc32c0
bhaskar-13 <53254229+bhaskar-13@users.noreply.github.com>
2021-09-22 12:51:14 +0530

    Create resume.js
    resume is created in json format in javascript

---
+++ resume.js
@@ -0,0 +1,50 @@
+obj={
+  "basics": {
+    "name": "Bhaskakr",
+    "label": "Full Stack Devloper",
+    "email": "itsmebhaskar100@gmail.com",
+    "phone": "9679573649",
+    "summary": "A summary of Bhaskar",
+    "location": {
+      "address": "Brookshabad",
+      "postalCode": "744112",
+      "city": "PortBlair",
+      "countryCode": "India",
+      "region": "Union Teritory"
+    },
+   
+  },
+  
+  "education": [{
+    "institution": "Srm University",
+    "url": "https://srmInstitute.edu.in/",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2015-6-22",
+    "endDate": "2019-05-12",
+    "score": "7.9",
+  }],
+  
+  "skills": [{
+    "name": "Web Development",
+    "level": "Begineer",
+    "keywords": [
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ]
+  }],
+  "languages": [{
+    "language": "English,Tamil,Hindi",
+   
+  }],
+  "interests": [{
+    "name": "Table Tennis"
+  }],
+  "projects": [{
+    "name": "Project1",
+    "description": "Smart Agriculture System Using Iot and Machine Learning"},{"name1":"Project2","description2":"Event Management Application" } ],
+    "declaration":{"descp":"I here by declare that the above stated information is true to the best of my knowledge and belief"}
+};
+console.log(obj.declaration);
+
